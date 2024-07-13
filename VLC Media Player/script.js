322298b35b3fddcd01cc307b4f8c41cece36d724
Sovan Ghosh <ghoshsovan538@gmail.com>
2024-07-13 12:53:35 +0530

    Playback, audio, fullscreen

--- VLC Media Player/script.js
+++ VLC Media Player/script.js
@@ -4,17 +4,67 @@ const volUp=document.querySelector("#vol-up");
 const volDown=document.querySelector("#vol-down");
 
 const speedUpHandler=function(){
-  alert("speed up was clicked");
+  const video=document.querySelector("video");
+  if(video===null)
+    return;
+  if(video.playbackRate === 2)
+    return;
+  const increaseSpeed=video.playbackRate + 0.25;
+  video.playbackRate=increaseSpeed;
+  // Toast
+  toast.textContent=increaseSpeed+"X";
+  toast.style.display="block";
+  setTimeout(function(){
+      toast.style.display="none";
+  },2000);
 }
 const speedDownHandler=function(){
-    alert("speed down was clicked");
+    const video=document.querySelector("video");
+    if(video===null){
+        return;
+    }
+    if(video.playbackRate === 0)
+        return;
+    const decreaseSpeed=video.playbackRate - 0.25;
+    video.playbackRate=decreaseSpeed;
+    // Toast
+    toast.textContent=decreaseSpeed+"X";
+    toast.style.display="block";
+    setTimeout(function(){
+        toast.style.display="none";
+    },2000);
 }
 const volUpHandler=function(){
-    alert("volume up was clicked");
+    const video=document.querySelector("video");
+    if(video === null)
+        return;
+    if(video.volume === 1)
+        return;
+    const increaseVol = video.volume + 0.2;
+    video.volume = increaseVol;
+    // Toast
+    toast.textContent=(increaseVol*100)-((increaseVol*100)%10)+"%";
+    toast.style.display="block";
+    setTimeout(function(){
+        toast.style.display="none";
+    },2000);
 }
 const volDownHandler=function(){
-    alert("volume down was clicked");
+    const video=document.querySelector("video");
+    if(video === null)
+        return;
+    if(video.volume < 0.2)
+        return;
+    const decreaseVol = video.volume - 0.2;
+    video.volume = decreaseVol;
+    // Toast
+    toast.textContent=(decreaseVol*100)-((decreaseVol*100)%10)+"%";
+    toast.style.display="block";
+    setTimeout(function(){
+        toast.style.display="none";
+    },2000);
 }
+const toast=document.querySelector(".toast");
 
 speedUp.addEventListener("click", speedUpHandler);
 speedDown.addEventListener("click", speedDownHandler);
@@ -42,7 +92,6 @@ const acceptInputHandler=function(obj){
     // video.controls="true";
     // video.muted="true";
     main.appendChild(video);
-    video.volume=70;
 }
 
 openFile.addEventListener("change",acceptInputHandler);
